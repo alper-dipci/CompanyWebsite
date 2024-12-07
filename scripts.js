86a473423634ef0db3f59bb1bb20a4427e834c38
soulfly1871 <oguzemin22@proton.me>
2024-12-08 01:49:13 +0300

    Product 1'e ödeme pop-up yerleştirdim.

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Show the payment modal when Buy Now button is clicked
+function showPaymentPopup() {
+  var paymentModal = new bootstrap.Modal(document.getElementById('paymentModal'));
+  paymentModal.show();
+}
