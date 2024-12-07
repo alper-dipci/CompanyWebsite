8f7ab18649900d50aa57b94d194e387de3320703
soulfly1871 <oguzemin22@proton.me>
2024-12-08 01:54:02 +0300

    product 1 fade animasyonu

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
-// Show the payment modal when Buy Now button is clicked
+// payment kısmı görünecek tıklanınca
 function showPaymentPopup() {
   var paymentModal = new bootstrap.Modal(document.getElementById('paymentModal'));
   paymentModal.show();
 }
+
+
+// animasyonlar
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+
+  // animasyon delay
+  cards.forEach((card, index) => {
+    setTimeout(() => {
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+      card.style.animation = "fadeIn 0.5s ease-in-out";
+    }, index * 200); // animasyon delay 200ms burada
+  });
+});
