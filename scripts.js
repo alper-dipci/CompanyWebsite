2419d61c9db17f0cd9b9db60e49f177cb23dee42
gorkem07e <gorkem07ergen@hotmail.com>
2024-12-14 22:40:28 +0300

    GoTopBtn eklendi

--- scripts.js
+++ scripts.js
@@ -18,3 +18,23 @@ document.addEventListener("DOMContentLoaded", () => {
     }, index * 200); // animasyon delay 200ms burada
   });
 });
+
+//go up btn
+const goTopBtn = document.querySelector('.go-top-btn');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 200) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
+}
+
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
